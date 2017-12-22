7a54cc50ce9e890234c90f4f7db6c77f7f451a7f
mliorentas <matas0911@gmail.com>
2017-12-22 10:12:25 +0200

    a

--- DigitalAudioWorkstation/DigitalAudioWorkstation/Program.cs
+++ DigitalAudioWorkstation/DigitalAudioWorkstation/Program.cs
@@ -1,4 +1,6 @@
-﻿using Autofac;
+﻿using ArchiMetrics.Analysis;
+using ArchiMetrics.Analysis.Common;
+using Autofac;
 using DigitalAudioWorkstation.Domain.Factories.Audio;
 using DigitalAudioWorkstation.Domain.Factories.Midi;
 using DigitalAudioWorkstation.Domain.Services;
@@ -8,6 +10,9 @@ using DigitalAudioWorkstation.Facade;
 using DigitalAudioWorkstation.Repository;
 using DigitalAudioWorkstation.UI.Controllers;
 using DigitalAudioWorkstation.UI.View;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DigitalAudioWorkstation
 {
@@ -33,7 +38,7 @@ namespace DigitalAudioWorkstation
             builder.RegisterType<InMemoryTrackStore>().As<ITrackStore>().SingleInstance();
 
             //Different realizations for different classes
-            builder.Register(ctx => new AudioTrackService(new AudioTrackFactory(), ctx.Resolve< ITrackStore>(), ctx.Resolve<IClipStore>())).As<ITrackService>();
+            builder.Register(ctx => new AudioTrackService(new AudioTrackFactory(), ctx.Resolve<ITrackStore>(), ctx.Resolve<IClipStore>())).As<ITrackService>();
             builder.Register(ctx => new AudioEffectService(new AudioEffectsFactory(), ctx.Resolve<IEffectStore>(), ctx.Resolve<IInstrumentStore>())).As<IEffectService>();
             builder.Register(ctx => new AudioDeviceService(new AudioDeviceFactory(), ctx.Resolve<IInputStore>(), ctx.Resolve<IOutputStore>())).As<IDeviceService>();
 
@@ -52,8 +57,29 @@ namespace DigitalAudioWorkstation
             Container = builder.Build();
 
             var view = Container.Resolve<MainView>();
+            var task = RunAnalysis();
+            task.Wait();
             view.Main();
 
+
+        }
+
+
+        private static async Task RunAnalysis()
+        {
+            Console.WriteLine("Loading Solution");
+            var solutionProvider = new SolutionProvider();
+            var solution = await solutionProvider.Get(@"C:\Users\Matas\Desktop\5 Semestras\prog-sis-proj\DigitalAudioWorkstation\DigitalAudioWorkstation.sln");
+            Console.WriteLine("Solution loaded");
+
+            var projects = solution.Projects.ToList();
+
+            Console.WriteLine("Loading metrics, wait it may take a while.");
+            var metricsCalculator = new CodeMetricsCalculator();
+            var calculateTasks = projects.Select(p => metricsCalculator.Calculate(p, solution));
+            var metrics = (await Task.WhenAll(calculateTasks)).SelectMany(nm => nm);
+            foreach (var metric in metrics)
+                Console.WriteLine("Package {0} => A {3}", metric.Name, metric.Abstractness);
         }
     }
 }
